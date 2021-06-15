0cb715cc92709752739b0f5ffcf802fbdaa6b362
JusticeShurup <69842449+JusticeShurup@users.noreply.github.com>
2021-06-15 20:30:05 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ std::mutex mutex_empty_data;
 std::mutex mutex_full_data;
 std::condition_variable no_data_condition;
 std::condition_variable collection_full;
+bool flag = true;
 void fib(int number) {
 	std::chrono::milliseconds(10);
 	int a = 1, b = 1, c;
@@ -23,21 +24,21 @@ void fib(int number) {
 	std::cout << c << std::endl;
 }
 
-void runThread(std::queue<int>& queue, std::queue<int>& all_elems) {
+void runThread(std::queue<int>& queue) {
 	std::unique_lock<std::mutex> lock_empty_data(mutex_empty_data);
-	while (all_elems.size() > 0) {
+	while (flag) {
 		while (queue.empty())
 			no_data_condition.wait(lock_empty_data);
 		fib(queue.front());
 		queue.pop();
-		all_elems.pop();
 		collection_full.notify_one();
 	}
 }
 
-void addElem(std::queue<int>& queue, std::ifstream& input) {
+void addElem(std::queue<int>& queue) {
 	int element;
 	std::unique_lock<std::mutex> lock_full_data(mutex_full_data);
+	std::ifstream input("input.txt");
 	while (input >> element) {
 		while (queue.size() >= 100) {
 			collection_full.wait(lock_full_data);
@@ -46,34 +47,25 @@ void addElem(std::queue<int>& queue, std::ifstream& input) {
 		no_data_condition.notify_one();
 		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
+	input.close();
+	flag = false;
+
 }
 
-void full_all_elems(std::queue<int>& all_elems) {
-	std::ifstream input("input.txt");
-	int element;
-	while (input >> element)
-		all_elems.push(element);
-	input.close();
-}
 
 int main() {
 	std::queue <int> queue;
-	std::ifstream input("input.txt");
 
-	std::queue <int> all_elems;
-	full_all_elems(all_elems);
-
-	std::thread producer(&addElem, std::ref(queue), std::ref(input));
+	std::thread producer(&addElem, std::ref(queue));
 
 	std::vector<std::thread> threads;
 	for (int i = 0; i < 4; i++) 
-		threads.push_back(std::thread(&runThread, std::ref(queue), std::ref(all_elems)));
+		threads.push_back(std::thread(&runThread, std::ref(queue)));
+	producer.join();
 
 	for (int i = 0; i < 4; i++) 
 		threads[i].join();
 
-	producer.join();
 
-	input.close();
 	return 0;
 }
